717c94573a199a0e029d47cd07f97c9e1e4dcdc5
SONITO-WA <walidar911@gmail.com>
2025-06-10 20:46:11 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,5 @@
 <?php
-  // Vérifier si la session n'est pas déjà démarrée
-  if (session_status() === PHP_SESSION_NONE) {
     session_start();
-  }
 ?>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
